64603d86cb171c8ddaf387d4cf599a1f86fa94ed
pawor <pawor.emi@gmail.com>
2022-02-17 00:35:54 +0700

    setup commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from 'axios'
+import { useState } from 'react'
 
 function App() {
+  const [itemCD, setItemCD] = useState('');
+  const [itemNM, setItemNM] = useState('');
+  const [model, setModel] = useState('');
+  const [user, setUser] = useState('');
+  const [time, setTime] = useState([]);
+
+  const getItems = () => {
+    Axios.get('http://localhost:3001/api').then((response) => {
+      setItemsList(response.data);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>LENS INVENTORY SYSTEM</h1>
+      <div className='information'>
+        <form action=''>
+          <div className='mb-3'>
+            <label htmlFor='ITEM_CD' className='form-label'>
+              ITEM NUMBER
+            </label>
+          </div>
+        </form>
+        <button className='btn btn-primary' onClick={getItems}>ITEMS LIST</button>
+        {itemsList.map((val,key) => {
+          return (
+            <div className='item card'>
+              <div className='card-body text-left'>
+                <p className='card-text'>ITEM NUMBER: {val.ITEM_CD}</p>
+                <p className='card-text'>ITEM NAME: {val.ITEM_NM}</p>
+                <p className='card-text'>MODEL: {val.Model}</p>
+                <p className='card-text'>USER NAME: {val.Users_update}</p>
+                <p className='card-text'>TIME: {val.Update_date}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
